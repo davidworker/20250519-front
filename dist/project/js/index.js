231c08953d2fff68053f98cfaa21966c21033f76
kindping <kindpingwork@gmail.com>
2025-08-13 20:19:52 +0800

    Footer 元件

--- dist/project/js/index.js
+++ dist/project/js/index.js
@@ -1,5 +1,7 @@
 import { Header } from "./components/Header.js";
 import { Favicon } from "./components/Favicon.js";
+import { Footer } from "./components/Footer.js";
 
 Header.init("#header-container", "index");
+Footer.init("#footer-container");
 Favicon.init();
